f16824864982f115c6663dcccb7ee5995d690f83
JustBru00 <justbru00@gmail.com>
2015-08-08 21:31:22 -0400

    Added some code.

--- src/com/gmail/justbru00/epic/smp/Main/Main.java
+++ src/com/gmail/justbru00/epic/smp/Main/Main.java
@@ -1,5 +1,33 @@
 package com.gmail.justbru00.epic.smp.Main;
 
-public class Main {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	public static String Prefix = "&8[&bEpic&fSMP&8]";
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+		return false;
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+	@Override
+	public void onEnable() {
+		enablePlugin();
+	}
+
+	
+	public void enablePlugin(){
+	
+	}
+	
 
 }
